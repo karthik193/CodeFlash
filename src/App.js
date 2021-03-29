bfd534d5b3db69dfae73d9b35eca5c86842063c7
Karthik Pasupulatei <karthik.pasupulatei@gmail.com>
2021-03-29 12:27:25 +0530

    project created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Compiler from  './components/Compiler/compiler' ; 
+function App() {
+  return (
+    <div className="App">
+      <Compiler />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Compiler/compiler.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Compiler (){
+    // requesting token to API 
+    // using token to send input and get the response 
+    // managing the UI 
+    return (
+        // provide UI to user
+        // taken inputs from the user
+        <div>Start your Code here</div>
+    );
+}
+export default Compiler;
